b08eded9ee2096eabcd61beffc9fdde8d082737b
Eugene Petrenko <eugene.petrenko@gmail.com>
2013-03-22 18:36:58 +0100

    initial project setup

---
+++ AudioBroadcastr/src/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace EugenePetrenko.AudioBroadcastr
+{
+  public class Program
+  {
+    [STAThread]
+    static void Main(string[] args)
+    {
+      Console.Out.WriteLine("This is an AudioBroadcastr to capture 'whatUhead' audio");
+      Console.Out.WriteLine("(C) Eugene Petrenko 2013");
+      Console.Out.WriteLine("");
+
+
+    }
+  }
+}
