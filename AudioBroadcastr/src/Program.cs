49441e1b3c04a2bea16f46c3638bab7e8abf307d
Eugene Petrenko <eugene.petrenko@gmail.com>
2013-03-22 19:25:52 +0100

    implement basic audio capturing

--- AudioBroadcastr/src/Program.cs
+++ AudioBroadcastr/src/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using NAudio.CoreAudioApi;
+using NAudio.Wave;
 
 namespace EugenePetrenko.AudioBroadcastr
 {
@@ -10,6 +12,31 @@ namespace EugenePetrenko.AudioBroadcastr
       Console.Out.WriteLine("This is an AudioBroadcastr to capture 'whatUhead' audio");
       Console.Out.WriteLine("(C) Eugene Petrenko 2013");
       Console.Out.WriteLine("");
+
+      Console.Out.WriteLine("Looking for audio capture devices:");
+      for (int n = 0; n < WaveIn.DeviceCount; n++)
+      {
+        var dev = WaveIn.GetCapabilities(n);
+        Console.Out.WriteLine("  Device: {0}, channels={1}", dev.ProductName, dev.Channels);
+      }
+
+      Console.Out.WriteLine("Looking for audio MMD:");
+      var mmd = new MMDeviceEnumerator();
+      var mddDev = mmd.GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia);
+      Console.Out.WriteLine("  [default] {0} {1}", mddDev.ID, mddDev.FriendlyName);
+
+      Console.Out.WriteLine("Starting from default device");
+      var capture = new WasapiLoopbackCapture();
+
+      int sz = 0;
+      capture.DataAvailable += (sender, eventArgs) =>
+        {
+          sz += eventArgs.BytesRecorded;
+          if (sz%1024 == 0) Console.Out.WriteLine(".");
+        };
+
+      Console.Out.WriteLine("Loopback wave format is: {0}", capture.WaveFormat);
+      capture.StartRecording();
 
 
     }
