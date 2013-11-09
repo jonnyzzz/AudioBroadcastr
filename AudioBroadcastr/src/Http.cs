6376a62e95580d52b7a95d173ca9641c5b294e00
Eugene Petrenko <eugene.petrenko@gmail.com>
2013-11-09 22:59:42 +0100

    better logging for HTTP

--- AudioBroadcastr/src/Http.cs
+++ AudioBroadcastr/src/Http.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Threading;
@@ -39,15 +40,15 @@ namespace EugenePetrenko.AudioBroadcastr
 
     private void ProcessRequest()
     {
+      int id = 10000;
       while (myIsRunning)
       {
         var newConn = myServer.AcceptTcpClient();
-
-        myPool.EnqueueTask(() => ProcessRequest(newConn));
+        myPool.EnqueueTask(() => ProcessRequest(id++, newConn));
       }
     }
 
-    private void ProcessRequest(TcpClient newConn)
+    private void ProcessRequest(int id, TcpClient newConn)
     {
       var iep = (IPEndPoint) newConn.Client.RemoteEndPoint;
       Console.WriteLine("Connected with a client: {0}: {1} ", iep.Address, iep.Port);
@@ -59,7 +60,14 @@ namespace EugenePetrenko.AudioBroadcastr
         var request = sr.ReadLine();
         if (request == null) return;
         
-        Console.Out.WriteLine("Request: {0}", request);
+        Console.Out.WriteLine();
+        Console.Out.WriteLine("{1} Request: {0}", request, id);
+        while (true)
+        {
+          var headers = sr.ReadLine();
+          if (headers == null || headers.Trim() == "") break;
+          Console.Out.WriteLine("{1}  {0}", headers, id);
+        }
         var path = request.Split(' ')[1];
 
         if (path.StartsWith("/mp3"))
@@ -71,7 +79,7 @@ namespace EugenePetrenko.AudioBroadcastr
           sw.WriteLine();
           sw.Flush();
 
-          HandleStreamingClient(sw.BaseStream);      
+          HandleStreamingClient(id, sw.BaseStream);      
         }
         else
         {
@@ -83,45 +91,71 @@ namespace EugenePetrenko.AudioBroadcastr
           sw.Write("No Data For you now\r\n");
           sw.Flush();
         }
-
-
-        //read all data from request
-        String line;
-        while ((line = sr.ReadLine()) != null)
-        {
-          if (line.Length == 0) break;
-        }
       }
     }
 
 
     private readonly ManualResetEvent myExitEvent = new ManualResetEvent(false);
 
-    private void HandleStreamingClient(Stream output)
+    private void HandleStreamingClient(int id, Stream output)
     {
-      var exitEvent = new ManualResetEvent(false);
-      var sw = NewClientProxy(output);
-
-      Action<byte[], int> handler = null;
-      handler = (bytes, i) =>
+      try
       {
-        try
-        {
-          sw.Write(bytes, 0, i);
-        }
-        catch(Exception e)
-        {
-          Console.Out.WriteLine(e);
-          OnData -= handler;
-          exitEvent.Set();
-        }
-      };
+        var exitEvent = new ManualResetEvent(false);
+        var sw = NewClientProxy(output);
 
-      OnData += handler;
-      
-      WaitHandle.WaitAny(new WaitHandle[]{myExitEvent, exitEvent});
+        Action<byte[], int> handler = null;
+        handler = (bytes, i) =>
+        {
+          try
+          {
+            sw.Write(bytes, 0, i);
+          }
+          catch (Exception e)
+          {
+            LogException(id, e);
+            OnData -= handler;
+            exitEvent.Set();
+          }
+        };
 
-      OnData -= handler;
+        OnData += handler;
+
+        WaitHandle.WaitAny(new WaitHandle[] {myExitEvent, exitEvent});
+
+        OnData -= handler;
+      }
+      catch (Exception e)
+      {
+        LogException(id, e);
+      }
+    }
+
+    private static void LogException(int id, Exception e)
+    {
+      if (e is IOException && e.InnerException != null)
+      {
+        LogException(id, e.InnerException);
+        return;
+      }
+
+      var innerSocket = e as SocketException;
+      if (innerSocket != null)
+      {
+        Console.Out.WriteLine(id + "  " + e.Message);
+        return;
+      }
+
+      Console.Out.WriteLine(id + " " + e.Message);
+      var lines = e.ToString()
+        .Split("\r\n".ToCharArray(), StringSplitOptions.RemoveEmptyEntries)
+        .Select(x => x.TrimEnd())
+        .Where(x => x.Length > 0);
+
+      foreach (var line in lines)
+      {
+        Console.Out.WriteLine(id + "    " + line);
+      }
     }
   }
 
