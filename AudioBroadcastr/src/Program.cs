c8cd0e7fd44934bd9018b5769cb98beec260e165
Eugene Petrenko <eugene.petrenko@gmail.com>
2013-11-09 23:01:16 +0100

    spelling

--- AudioBroadcastr/src/Program.cs
+++ AudioBroadcastr/src/Program.cs
@@ -14,7 +14,7 @@ namespace EugenePetrenko.AudioBroadcastr
     [STAThread]
     static void Main(string[] args)
     {
-      Console.Out.WriteLine("This is an AudioBroadcastr to capture 'whatUhead' audio");
+      Console.Out.WriteLine("This is an AudioBroadcastr to capture 'whatUhear' audio");
       Console.Out.WriteLine("(C) Eugene Petrenko 2013");
       Console.Out.WriteLine("");
 
